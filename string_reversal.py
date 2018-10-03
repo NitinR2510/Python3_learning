66e3720061a6eacb0fee5e19e44c2be3b498e79e
NitinR2510 <43118810+NitinR2510@users.noreply.github.com>
2018-10-03 22:02:16 +0530

    Update_py_1

--- string_reversal.py
+++ string_reversal.py
@@ -1,5 +1,6 @@
-a = eval(input())
-b = input()
+#the program accepts number oof characters in strings, the string... and then reverses it as output.
+a = eval(input())  #specifies the number of characters to be entered in the string or number to be reversed
+b = input()   #the actual string that has to undergo reversion. It may be a number as well.
 c = a-1
 d = ''
 while c>=0:
